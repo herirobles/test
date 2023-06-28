e9e5f1e5b00a9b14e9534fb42aaf4d16d9ccb868
herirobles <137955943+herirobles@users.noreply.github.com>
2023-06-28 07:48:09 -0600

    Add files via upload

--- appwithdata.py
+++ appwithdata.py
@@ -1,27 +1,34 @@
 import mysql.connector
 
-# Connect to the database
-conn = mysql.connector.connect(
-    host='db-mysql-tor1-59654-do-user-14245628-0.b.db.ondigitalocean.com',
-    port= 25060,
-    user='heri2',
-    password='AVNS__EO7b3yqnmDshkXmeTZ',
-    database='pets_images'
-)
+try:
+    # Connect to the database
+    conn = mysql.connector.connect(
+        host='db-mysql-tor1-59654-do-user-14245628-0.b.db.ondigitalocean.com',
+        port=25060,
+        user='heri2',
+        password='AVNS__EO7b3yqnmDshkXmeTZ',
+        database='pets_images'
+    )
 
-# Create a cursor object to execute SQL queries
-cursor = conn.cursor()
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
 
-# Execute a select query to fetch all rows from a table
-cursor.execute('SELECT * FROM Pets')  # Replace 'your_table' with the name of your actual table
+    # Execute a select query to fetch all rows from a table
+    cursor.execute('SELECT * FROM Pets')  # Replace 'Pets' with the name of your actual table
 
-# Fetch all the rows returned by the query
-rows = cursor.fetchall()
+    # Fetch all the rows returned by the query
+    rows = cursor.fetchall()
 
-# Display the content of the database
-for row in rows:
-    print(row)  # Modify this line to format and print the content as desired
+    # Display the content of the database
+    for row in rows:
+        print(row)  # Modify this line to format and print the content as desired
 
-# Close the cursor and the database connection
-cursor.close()
-conn.close()
+except mysql.connector.Error as error:
+    print("Error connecting to the database:", error)
+
+finally:
+    # Close the cursor and the database connection
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
